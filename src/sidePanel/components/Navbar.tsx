4ecbe5860c456a48a473ca318a690890232ce8b0
JasonCarmichael4 <jaseque999@gmail.com>
2025-10-28 09:50:07 +0000

    reorder navbar links

--- src/sidePanel/components/Navbar.tsx
+++ src/sidePanel/components/Navbar.tsx
@@ -5,11 +5,11 @@ import { NavLink } from 'react-router-dom';
 const SideNavbar: React.FC = () => (
     <Navbar>
         <Nav>
-            <Nav.Link as={NavLink} to="/info">Info</Nav.Link>
-            <Nav.Link as={NavLink} to="/citation">Citation</Nav.Link>
             <Nav.Link as={NavLink} to="/flashcard">Flashcard</Nav.Link>
+            <Nav.Link as={NavLink} to="/citation">Citation</Nav.Link>
             <Nav.Link as={NavLink} to="/quiz">Quiz</Nav.Link>
             <Nav.Link as={NavLink} to="/summary">Summary</Nav.Link>
+            <Nav.Link as={NavLink} to="/info">Info</Nav.Link>
         </Nav>
     </Navbar>
 );
