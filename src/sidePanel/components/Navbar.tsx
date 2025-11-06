708ae88ee72fabc8638b8cc477b1cf9eddf25630
MiguelProgramas <miguel.p.rograma.lima.tsis@gmail.com>
2025-11-06 00:13:50 -0600

    :tada: First commit! Basic navbar styling.

--- src/sidePanel/components/Navbar.tsx
+++ src/sidePanel/components/Navbar.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import styles from "./navbar.module.css";
 
 const SideNavbar: React.FC = () => (
-    <Navbar>
-        <Nav>
-            <Nav.Link as={NavLink} to="/flashcard">Flashcard</Nav.Link>
-            <Nav.Link as={NavLink} to="/citation">Citation</Nav.Link>
-            <Nav.Link as={NavLink} to="/quiz">Quiz</Nav.Link>
-            <Nav.Link as={NavLink} to="/summary">Summary</Nav.Link>
-            <Nav.Link as={NavLink} to="/info">Info</Nav.Link>
+    <Navbar className={styles.navBarWrapper}>
+        <Nav className={styles.navBar}>
+            <Nav.Link as={NavLink} to="/flashcard" style={({isActive}) => ({
+                backgroundColor: isActive ? 'white' : 'darkgrey',
+                borderBottom: isActive ? 'none' : '',
+                color: isActive ? '#582d03' : ''
+            })} className={styles.navBarItems}>Flashcard</Nav.Link>
+            <Nav.Link as={NavLink} to="/citation" style={({isActive}) => ({
+                backgroundColor: isActive ? 'white' : 'darkgrey',
+                borderBottom: isActive ? 'none' : '',
+                color: isActive ? '#582d03' : ''
+            })} className={styles.navBarItems}>Citation</Nav.Link>
+            <Nav.Link as={NavLink} to="/quiz" style={({isActive}) => ({
+                backgroundColor: isActive ? 'white' : 'darkgrey',
+                borderBottom: isActive ? 'none' : '',
+                color: isActive ? '#582d03' : ''
+            })} className={styles.navBarItems}>Quiz</Nav.Link>
+            <Nav.Link as={NavLink} to="/summary" style={({isActive}) => ({
+                backgroundColor: isActive ? 'white' : 'darkgrey',
+                borderBottom: isActive ? 'none' : '',
+                color: isActive ? '#582d03' : ''
+            })} className={styles.navBarItems}>Summary</Nav.Link>
+            <Nav.Link as={NavLink} to="/info" style={({isActive}) => ({
+                backgroundColor: isActive ? 'white' : 'darkgrey',
+                borderBottom: isActive ? 'none' : '',
+                color: isActive ? '#582d03' : ''
+            })} className={styles.navBarItems}>Info</Nav.Link>
         </Nav>
     </Navbar>
 );
